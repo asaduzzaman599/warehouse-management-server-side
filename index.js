5dd7c3ddcc56e6f76e6df0bdf16ef3342156c9b9
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-04-29 03:04:38 +0600

    mongo db connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -8,6 +9,27 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors())
 app.use(express.json())
+
+//mongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wls22.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+(async () => {
+    try {
+        await client.connect()
+        console.log("DB Connected")
+    } catch (err) {
+        console.log(err)
+    }
+})().catch(console.dir)
+
+
+/* 
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+}); */
+
 
 app.get('/', (req, res) => {
     res.send(`Server running port : ${port}`)
