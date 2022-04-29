3dc2d821047594efaddcac58cd2a63b2c9956690
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-04-29 22:14:35 +0600

    one item api created

--- index.js
+++ index.js
@@ -20,15 +20,31 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
         const collectionProduct = client.db('store_house').collection('product')
 
         //get product data from db
-        app.get('/product', async (req, res) => {
+        app.get('/products', async (req, res) => {
+            const limit = +req?.query?.limit || 0
             const query = {}
             //get product from db
-            const result = await collectionProduct.find(query).limit(0).toArray();
+            const result = await collectionProduct.find(query).limit(limit).toArray();
             res.send(result)
-
 
         })
 
+        //get one product
+        app.get('/product/:productId', async (req, res) => {
+            const id = req?.params?.productId
+            const query = {
+                _id: ObjectId(id)
+            }
+            console.log(id)
+            //get product from db
+            const result = await collectionProduct.findOne(query);
+
+            if (result?._id) {
+                res.send({ success: true, data: result })
+            } else {
+                res.send({ success: false, error: "Something is Wrong" })
+            }
+        })
 
         //get product data depend on email  from db with jwt token verification
         app.get('/myproduct', async (req, res) => {
