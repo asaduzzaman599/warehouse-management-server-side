cd4b73c17852f7f6dbefb465490cc597dadc7e91
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-04-30 14:29:07 +0600

    JWT added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 const app = express()
 const port = process.env.PORT || 5000;
